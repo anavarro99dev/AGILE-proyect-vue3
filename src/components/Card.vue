bcd6fdce69f0ba361a6891b698a227fe8da6ce83
anavarro99dev <anavarro.develop99@gmail.com>
2023-01-04 22:42:28 -0500

    Card#4 Coreccion

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card-container">
     <div class="card-header">
-      <span class="card-header--icon"> </span>
+      <div class="card-header--icon"><span> </span></div>
       <div class="card-header--city">
         <h3>Washington DC</h3>
         <span>38.9072° N, 77.0369° W</span>
@@ -33,52 +33,54 @@
 
 <style lang="scss" scoped>
 .card-container {
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: auto auto auto;
+  justify-items: center;
+  align-items: center;
+
   background-color: #023047;
   border: solid 0.1rem #000000;
   border-radius: 2rem;
   width: 90vw;
-  height: 17rem;
+  height: 25rem;
   margin: 1rem auto;
 }
 
 .card-header {
   display: flex;
+  align-content: space-between;
+  width: inherit;
   justify-content: space-evenly;
-  font-size: 1rem;
-  margin: 1rem;
-  span{
-    font-size: 0.95rem;
+  span {
+    font-size: 1.2rem;
+  }
+  h3 {
+    font-size: 2rem;
   }
 }
 
 .card-header--icon {
-  width: 2.5rem;
-  height: 2.5rem;
+  width: 5rem;
+  height: 5rem;
   background-color: white;
-  align-self: center;
 }
 
 .card--body {
-  margin: 2rem 0;
+  font-size: 1.6rem;
   table {
-    margin: 0 auto;
-    font-size: 0.95rem;
   }
   span {
     margin-left: 0.2rem;
-  }
-  h3{
-    font-size: 2rem;
   }
 }
 .content::after {
   content: " | ";
 }
 
-.card--icon-delete{
-  width: 2.5rem;
-  height: 2.5rem;
+.card--icon-delete {
+  width: 3rem;
+  height: 3rem;
   background-color: white;
-  margin: 0 auto;
 }
 </style>
