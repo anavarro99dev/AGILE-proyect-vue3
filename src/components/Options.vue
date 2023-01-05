dbf6ed155b67088f9f8cc40d9e8067cfa2f9ce6f
anavarro99dev <anavarro.develop99@gmail.com>
2023-01-04 21:50:00 -0500

    Option#2 coreccion margen

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -18,38 +18,38 @@ import Switch from "./Switch.vue";
 
 <style lang="scss" scoped>
 .option-container {
-  .option--units-select {
-    display: flex;
-    justify-content: flex-end;
-    padding: 0.5rem 0.5rem;
+  width: 90vw;
+  margin: 0 auto;
+}
+.option--units-select {
+  display: flex;
+  justify-content: flex-end;
+  padding: 0.5rem 0.5rem;
+}
+.option--city {
+  display: flex;
+  justify-content: space-around;
+  button {
+    background-color: #219ebc;
+    width: 4.5rem;
+    height: 3rem;
+    border: none;
+    border-radius: 2rem;
   }
-  .option--city {
-    display: flex;
-    justify-content: space-around;
-    .option--input-container {
-      border-style: none;
-      background-color: #ecececfa;
-      height: 3rem;
-      border-radius: 0.5rem;
-      display: flex;
-      align-items: center;
-      padding: 0 1rem;
-      input {
-        border: none;
-        background-color: inherit;
-        &:focus {
-          outline: none;
-        }
-      }
-    }
-    button {
-      background-color: #219ebc;
-      width: 5.5rem;
-      height: 3rem;
-      border: none;
-      border-radius: 2rem;
-      &:focus {
-      }
+}
+.option--input-container {
+  border-style: none;
+  background-color: #ecececfa;
+  height: 3rem;
+  border-radius: 0.5rem;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  input {
+    border: none;
+    background-color: inherit;
+    &:focus {
+      outline: none;
     }
   }
 }
